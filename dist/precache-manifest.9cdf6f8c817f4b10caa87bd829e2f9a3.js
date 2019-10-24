b48ae50f2a31e473f747d5b8456ebf130d203d2a
Sheep <ht159159@gmail.com>
2019-10-24 17:02:54 +0800

    dist

---
+++ dist/precache-manifest.9cdf6f8c817f4b10caa87bd829e2f9a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815c658030a1b4972900",
+    "url": "/css/app.d002e51a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6386c1e1e2a3234b4db87b51219afeb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43a38a48d7d1821b7a7a",
+    "url": "/js/about.beacf424.js"
+  },
+  {
+    "revision": "815c658030a1b4972900",
+    "url": "/js/app.f7d336fd.js"
+  },
+  {
+    "revision": "3e9e93db744c65429f8d",
+    "url": "/js/chunk-vendors.3eb09af2.js"
+  },
+  {
+    "revision": "cdf2980dfb06988f644d67e09bcf6854",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
